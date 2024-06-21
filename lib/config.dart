be465f9097a4e3091ad052cc1f9eec5fe495d2e9
Gal Ogdan <telecarproj@gmail.com>
2024-06-21 19:12:01 +0300

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,5 @@
-String currentIP = 'http://192.168.1.100:8000';
-
-String local = 'http://192.168.1.100:8000';
+String currentIP = 'LOCAL IP TO USE';
 
 
-String cloud = 'https://fastapi-app-qizpt4l6tq-zf.a.run.app';
+
+String cloud = 'Our Cloud URL';
